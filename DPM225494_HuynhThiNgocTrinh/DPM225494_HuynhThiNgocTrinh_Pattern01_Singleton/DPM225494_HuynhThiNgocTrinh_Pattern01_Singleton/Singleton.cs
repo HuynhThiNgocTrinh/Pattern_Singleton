5d1164517c320a81430a709d4eacd264f9cedec6
HuynhThiNgocTrinh <trinh_dpm225494@student.agu.edu.vn>
2025-09-08 11:01:12 +0700

    Add files via upload

---
+++ DPM225494_HuynhThiNgocTrinh/DPM225494_HuynhThiNgocTrinh_Pattern01_Singleton/DPM225494_HuynhThiNgocTrinh_Pattern01_Singleton/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225494_HuynhThiNgocTrinh_Pattern01_Singleton
+{
+    internal class Singleton
+    {
+        static Singleton instance;
+        protected Singleton()
+        {
+        }
+        public static Singleton Instance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+}
