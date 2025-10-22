64f3fd16f485e48e6c3ae477254dc37cf2ee143e
HuynhThiNgocTrinh <trinh_dpm225494@student.agu.edu.vn>
2025-10-22 08:11:56 +0700

    Add files via upload

---
+++ 4Remove Control Flag solution/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 2, 4, 6, 8, 10 };
+        int target = 6;
+
+        foreach (int num in numbers)
+        {
+            if (num == target)
+            {
+                Console.WriteLine("Found target: " + target);
+                break; // no need for control flag
+            }
+        }
+    }
+}
