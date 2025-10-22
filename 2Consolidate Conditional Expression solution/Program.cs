88ae54698d21e417f2b2f2e940aeb635e437edec
HuynhThiNgocTrinh <trinh_dpm225494@student.agu.edu.vn>
2025-10-22 08:10:24 +0700

    Add files via upload

---
+++ 2Consolidate Conditional Expression solution/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double result = DisabilityAmount(1.5, 8, false);
+        Console.WriteLine($"Disability Amount: {result}");
+    }
+
+    static double DisabilityAmount(double seniority, int monthsDisabled, bool isPartTime)
+    {
+        if (IsNotEligibleForDisability(seniority, monthsDisabled, isPartTime))
+        {
+            return 0;
+        }
+
+        // Tính toán số tiền trợ cấp (giả sử)
+        return 5000;
+    }
+
+    static bool IsNotEligibleForDisability(double seniority, int monthsDisabled, bool isPartTime)
+    {
+        return seniority < 2 || monthsDisabled > 12 || isPartTime;
+    }
+}
